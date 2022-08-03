f260a899ca683d8904197e3c6165a4b3ed57a6b8
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2022-08-03 18:38:11 -0300

    :sparkles: feat: Importando TaskService para TasksModule

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { TasksService } from './shared';
 
 @NgModule({
   declarations: [],
+  // providers: [TasksService], Esse serviço não precisa dessa importação de providers, pois ele já teve esse serviço injetado pelo Angular no componente através do Injectable.
   imports: [
     CommonModule
   ]
