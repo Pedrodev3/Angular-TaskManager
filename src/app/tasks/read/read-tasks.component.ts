658a20af74563b06a44e24d492c68db7c857e003
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2022-09-06 18:28:46 -0300

    :sparkles: bring mock related with new tasks - Involve read-tasks.component.ts

--- src/app/tasks/read/read-tasks.component.ts
+++ src/app/tasks/read/read-tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TasksService, Task } from '../shared';
 
 @Component({
   selector: 'app-read-tasks',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadTasksComponent implements OnInit {
 
-  constructor() { }
+  // A tipagem desse Array é a Class Task do componente "task.model.ts"
+  // Importando array "Task" que vem do "task.model.ts" do diretório "shared"
+  tasks: Task[];
 
+  constructor(private tasksService: TasksService) { }
+
+  // Utilizará do lifecycle hook "ngOnInit" para sempre iniciar listando todas as propriedades do objeto "localStorage"
   ngOnInit(): void {
+    this.tasks = this.readAll();
+    this.tasks = [
+      new Task(1, "Tarefa 1", false),
+      new Task(2, "Tarefa 2", true),
+    ];
+  }
+
+  /* Função para pegar o método "readAll" do componente Service e trazer para o Component/Controller para ele controlar o acesso entre a regra de negócio e a "view" da aplicação, o html, a parte que o usuário pode ver e interagir
+  É possível utilizar desse método pelo import feito em cima do "TasksService" */
+  readAll(): Task[] {
+    return this.tasksService.readAll();
   }
 
 }
