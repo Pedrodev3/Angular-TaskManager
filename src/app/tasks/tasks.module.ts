8f5ee7c8aa6b3e20efca550339ef586f1e97866a
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2022-09-06 17:58:14 -0300

    :sparkles: import two new modules RouterModule FormsModule

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { TasksService } from './shared';
 import { ReadTasksComponent } from './read'; // Só colocar o ".read" pois o index.ts já cuida do resto dos export
@@ -7,6 +9,6 @@ import { ReadTasksComponent } from './read'; // Só colocar o ".read" pois o ind
 @NgModule({
   declarations: [ReadTasksComponent],
   // providers: [TasksService], Esse serviço não precisa dessa importação de providers, pois ele já teve esse serviço injetado pelo Angular no componente através do Injectable.
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
 })
 export class TasksModule {}
